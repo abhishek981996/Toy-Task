d97ca1e85f8ed0857e39bf477da01ecf153d3475
abhishek981996 <abhishektiwari981996@gmail.com>
2018-02-26 16:21:02 +0530

    removed migrations

--- toy/models.py
+++ toy/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Cases(models.Model):
 
-	caseTitle = models.CharField(max_length=50)
+	caseTitle = models.CharField(max_length=20)
 	DOJ = models.DateField()
 	body = models.TextField()
 	def __str__(self):
